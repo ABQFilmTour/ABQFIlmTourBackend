a2df60e8715ce0732e9c42287ff863f122b29a27
samuelandazola <szola3@icloud.com>
2018-11-18 11:18:07 -0700

    update image dao

--- src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/dao/ImageRepository.java
+++ src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/dao/ImageRepository.java
@@ -1,5 +1,16 @@
 package edu.cnm.deepdive.abq_film_tour_backend.model.dao;
 
-public interface ImageRepository {
+import edu.cnm.deepdive.abq_film_tour_backend.model.entity.Image;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
+public interface ImageRepository {
+  @Query(value = "SELECT * FROM Image "
+      + "WHERE film_location_id = :filmLocationId ORDER BY timestamp",
+      nativeQuery = true)
+  List<Image> selectByLocation(long filmLocationId);
+
+  @Query(value = "SELECT * FROM Image WHERE image_id = :imageId ORDER BY timestamp",
+      nativeQuery = true)
+  List<Image> selectByUser(long imageId);
 }
--- src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/entity/Image.java
+++ src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/entity/Image.java
@@ -88,5 +88,5 @@ public class Image {
     this.source = source;
   }
 
-  
+
 }
