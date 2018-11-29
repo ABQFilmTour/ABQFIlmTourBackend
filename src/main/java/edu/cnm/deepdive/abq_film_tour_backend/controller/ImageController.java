e30231d65e7315ecc68e5164630df7fcb87cbdc4
samuelandazola <szola3@icloud.com>
2018-11-29 09:58:05 -0700

    update image controller

--- src/main/java/edu/cnm/deepdive/abq_film_tour_backend/controller/ImageController.java
+++ src/main/java/edu/cnm/deepdive/abq_film_tour_backend/controller/ImageController.java
@@ -6,13 +6,17 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,5 +45,12 @@ public class ImageController {
   public Image get(@PathVariable("imageId") UUID imageId){
     return imageRepository.findById(imageId).get();
   }
+
+  @DeleteMapping(value = "{imageId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable("imageId") UUID imageId){
+    imageRepository.deleteById(imageId);
+  }
+
 
 }
