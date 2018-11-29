1b9accc7a16c63add890dea8a2bc471acc586ab9
samuelandazola <szola3@icloud.com>
2018-11-29 10:27:13 -0700

    update user controller with getMapping, postMapping, deleteMapping

--- src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/dao/UserRepository.java
+++ src/main/java/edu/cnm/deepdive/abq_film_tour_backend/model/dao/UserRepository.java
@@ -1,9 +1,13 @@
 package edu.cnm.deepdive.abq_film_tour_backend.model.dao;
 
 import edu.cnm.deepdive.abq_film_tour_backend.model.entity.FilmLocation;
+import edu.cnm.deepdive.abq_film_tour_backend.model.entity.User;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<FilmLocation, UUID> {
+public interface UserRepository extends CrudRepository<User, UUID> {
+
+  List<User> findAllByOrderByFilmlocationAsc();
 
 }
